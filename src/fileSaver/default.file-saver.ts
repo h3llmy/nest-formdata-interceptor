41e75d85310d01b318adb35af03a33bf044186d0
H3llmy <helmytrisna86@gmail.com>
2024-06-05 11:11:36 +0800

    test: add defaultFileSaver unit test

--- src/fileSaver/default.file-saver.ts
+++ src/fileSaver/default.file-saver.ts
@@ -2,14 +2,29 @@ import path from "path";
 import { FileData, IFileSaver } from "../interfaces/file.interface";
 import fs from "fs";
 
+/**
+ * Default implementation of the IFileSaver interface.
+ * This class is responsible for saving file data to the filesystem.
+ */
 export class DefaultFileSaver implements IFileSaver {
-  save(fileData: FileData) {
+  /**
+   * Saves the provided file data to the specified file path.
+   * Ensures the directory exists and writes the file buffer to the file path.
+   *
+   * @param fileData - The file data to save.
+   * @returns The file path where the file was saved.
+   */
+  save(fileData: FileData): string {
     const directory = path.dirname(fileData.filePath);
+
+    // Ensure the directory exists
     if (!fs.existsSync(directory)) {
       fs.mkdirSync(directory, { recursive: true });
     }
 
+    // Write the file buffer to the specified file path
     fs.writeFileSync(fileData.filePath, fileData.buffer);
+
     return fileData.filePath;
   }
 }
