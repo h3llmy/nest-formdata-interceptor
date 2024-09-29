914ac0194d6a80c6ddc3e63e79dc8640b7b7ad06
H3llmy <helmytrisna86@gmail.com>
2024-09-29 13:15:46 +0800

    fix: FileData class generic type

--- src/classes/FileData.ts
+++ src/classes/FileData.ts
@@ -3,7 +3,7 @@ import { MimeType } from "../interfaces/file.interface";
 /**
  * Represents the data for a file upload.
  */
-export class FileData<ReturnType = string, SavePayloadType = undefined> {
+export class FileData<ReturnType = string, SavePayloadType = void> {
   constructor(
     public originalFileName: string,
     public fileName: string,
@@ -17,9 +17,11 @@ export class FileData<ReturnType = string, SavePayloadType = undefined> {
   ) {}
 
   /**
-   * Saves the file data to the data storage.
+   * Saves the file data to the specified location.
+   * @param args the payload sent to the custom file saver
+   * @returns the file path where the file was saved
    */
-  save(args?: SavePayloadType): ReturnType {
+  save(args: SavePayloadType): ReturnType {
     return "" as ReturnType;
   }
 }
