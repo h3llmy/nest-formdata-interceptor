335f07e0f3b0f4ba662f85e590c52f15c08850ac
H3llmy <helmytrisna86@gmail.com>
2024-09-29 16:39:16 +0800

    fix: duplicate export in main file

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export * from "./interceptors/formdata.interceptor";
 export * from "./interfaces/file.interface";
 
 export * from "./validators/isFileData.decorator";
-export * from "./validators/isFileData.decorator";
 export * from "./validators/minFileSize.decorator";
 export * from "./validators/maxFileSize.decorator";
 export * from "./validators/hasMimeType.decorator";
