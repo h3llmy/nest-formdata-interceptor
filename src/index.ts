8c56af03e91cbb5cac8d61a7f9e2745f58161f9f
h3llmy <helmytrisna86@gmail.com>
2025-12-19 14:27:06 +0000

    feat: make class-validator and calass-transformer package optional

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 export * from "./interceptors/formdata.interceptor";
 export * from "./interfaces/file.interface";
 
-export * from "./validators/isFileData.decorator";
-export * from "./validators/minFileSize.decorator";
-export * from "./validators/maxFileSize.decorator";
-export * from "./validators/hasMimeType.decorator";
-
 export * from "./classes/FileData";
 export * from "./classes/MultipleFileData";
